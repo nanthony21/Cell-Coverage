fdcf528175c945edd87d301f8efc50f4ef0a9084
Nick Anthony <nicholas.anthony@northwestern.edu>
2018-11-14 15:45:20 -0600

    Remove commented out code for old error fixes

--- flat_field_analyze.py
+++ flat_field_analyze.py
@@ -136,36 +136,12 @@ for plate_folder in plate_folder_list:
             background_area += np.count_nonzero(morph_img == 1)
             cell_area += np.count_nonzero(morph_img == 0)
             
-            # flip orientation for stitching
-#            morph_img = cv.flip(morph_img, 0)
-#            corr_img = cv.flip(corr_img, 0)
-#            outline = cv.flip(outline, 0)
-#            morph_img = np.rot90(morph_img)
-#            corr_img = np.rot90(corr_img)
-#            outline = np.rot90(outline)
-            
             # Write images to file
             cv.imwrite(osp.join(analyzed_folder, well_folder + '_' + binary_folder, analyzed_filename + cell_img_loc.split(well_folder)[2]), morph_img)
             cv.imwrite(osp.join(analyzed_folder,well_folder + '_' + ff_corr_folder, analyzed_filename + cell_img_loc.split(well_folder)[2]), corr_img)
             # Add segmentation outline to corrected image
             corr_img[outline.astype(bool)] = 0
             cv.imwrite(osp.join(analyzed_folder, well_folder + '_' + outline_folder, analyzed_filename + cell_img_loc.split(well_folder)[2]), corr_img)
-            
-            # Code to deal with snaking error 
-#            cell_labels = cell_img_loc.split('.')[0].split('_')
-#            cell_num = file_list[-1].split('.')[0].split('_')[6]
-#            temp_num_list = num_list[:num_list.index(cell_num)+1]
-#            temp_num_list.reverse()
-#            relabel_num = temp_num_list[num_list.index(cell_labels[6])]
-#            
-#            if int(cell_labels[7])%2 == 0:
-#                # write image to file
-#                cv.imwrite((analyzed_folder + '\\' + well_folder + '_' + binary_folder + '\\' + analyzed_filename + cell_img_loc.split(well_folder)[2]), morph_img)
-#                cv.imwrite((analyzed_folder + '\\' + well_folder + '_' + outline_folder + '\\' + analyzed_filename + cell_img_loc.split(well_folder)[2]), corr_img)
-#            else:
-#                # For odd rows flip image order
-#                cv.imwrite((analyzed_folder + well_folder + '_' + binary_folder + '\\' + analyzed_filename + file_prefix + relabel_num + '_' + cell_labels[7] + '.ome.tif') , morph_img)
-#                cv.imwrite((analyzed_folder + well_folder + '_' + outline_folder + '\\' + analyzed_filename + file_prefix + relabel_num + '_' + cell_labels[7] + '.ome.tif'), corr_img)
         
         # Output and save coverage numbers
         print('The coverage is ', 100*cell_area/(cell_area + background_area), ' %')
